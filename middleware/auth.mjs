e672112fe0d9b74277d8ba74c2b0a98551068090
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-24 16:56:16 -0700

    update user authorization

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -2,17 +2,17 @@ import jwt from 'jsonwebtoken';
 
 
 export const checkToken = (req, res, next) => {
-
-    const token = req.headers.authorization;
+    // Extract the token from cookies
+    const token = req.cookies.token; // Assuming the cookie's name is 'token'
     if (!token) {
         return res.status(401).json({ message: 'Authorization token is required' });
     }
 
     try {
-
+        // Verify the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-        next(); 
+        req.user = decoded; // Make sure this matches how you've structured your payload. Previously, it was decoded.user
+        next();
     } catch (error) {
         console.error(error);
         return res.status(401).json({ message: 'Invalid token' });
--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -71,11 +71,19 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: "Invalid credentials" });
         }
 
-        // If password matches, generate and return a JWT
+        // If password matches, generate a JWT
         const payload = { id: user._id, username: user.username };
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.json({ message: "Login successful", token: token });
+        // Set the token in an httpOnly cookie
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',
+            maxAge: 3600000
+        });
+
+        res.json({ message: "Login successful" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
