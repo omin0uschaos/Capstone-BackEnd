9a597beed863e75463e0a67658df7d541b8e2a50
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-26 19:19:35 -0700

    Update auth.mjs

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -19,22 +19,22 @@ export const checkToken = (req, res, next) => {
 };
 
 export const checkAdminToken = (req, res, next) => {
-    
     const token = req.headers.authorization;
     if (!token) {
       return res.status(401).json({ message: 'Authorization token is required' });
     }
-  
+
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      if (!decoded.user.isAdmin) {
+
+      if (!decoded.isAdmin) {
         return res.status(403).json({ message: 'Admin access required' });
       }
-  
-      req.user = decoded.user;
+
+      req.user = decoded;
       next();
     } catch (error) {
       console.error(error);
       return res.status(401).json({ message: 'Invalid token' });
     }
-  };
+};
